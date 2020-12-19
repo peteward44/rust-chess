880fc6bd9e980b547874613603c5f242a88994c2
pete <peteward44@gmail.com>
2020-12-19 23:39:07 +0000

    before 0.4.0 upgrade

--- client/src/sprite_picker.rs
+++ client/src/sprite_picker.rs
@@ -12,18 +12,29 @@ struct State {
 struct MouseLoc(Vec2);
 struct WindowSize(Vec2);
 
-// trait to attach to camera bundle
+/// Trait to attach to the camera bundle you wish to track
+/// ```
+/// .spawn(Camera2dComponents::default())
+///		.with(sprite_picker::SpritePickerCamera)
+/// ```
 pub struct SpritePickerCamera;
 
-// plugin
+
+/// Plugin class that should be added to your app builder
+/// ```
+/// builder.add_plugin(SpritePickerPlugin)
+/// ```
 pub struct SpritePickerPlugin;
 
-// event
+/// Mouse click event
+/// "name" is the name passed to the SpritePicker trait, "button" and "state" are the same from bevy's
+/// event system, and "pos" is the relative position in the hit area that the click occurred, from the centre of the area.
 #[derive(Debug)]
 pub struct MouseClick {
 	name: String,
 	button: MouseButton,
 	state: ElementState,
+	pos: Vec2,
 }
 
 impl Plugin for SpritePickerPlugin {
@@ -38,7 +49,10 @@ impl Plugin for SpritePickerPlugin {
 	}
 }
 
-// trait to add to sprites
+/// Add this trait to the sprites that you wish to receive click events
+/// ```
+/// .with(SpritePicker::new("my_sprite"));
+/// ```
 pub struct SpritePicker {
 	name: String,
 }
@@ -51,6 +65,49 @@ impl SpritePicker {
 	}
 }
 
+
+pub struct HitArea {
+	name: String,
+	size: Vec2,
+}
+
+impl HitArea {
+	pub fn new(name: &str, size: &Vec2) -> Self {
+		HitArea {
+			name: name.to_owned(),
+			size: size.clone(),
+		}
+	}
+}
+
+
+fn process_hitarea(
+	name: &String,
+	size: &Vec2,
+	hitarea_matrix: &Mat4,
+	point: &Vec3,
+	event: &MouseButtonInput,
+	my_events: &mut ResMut<Events<MouseClick>>,
+) {
+	let vec = hitarea_matrix.transform_point3(*point);
+
+	let half_width = size.x() / 2.0;
+	let half_height = size.y() / 2.0;
+	if vec.x() >= -half_width
+		&& vec.x() < half_width
+		&& vec.y() >= -half_height
+		&& vec.y() < half_height
+	{
+		my_events.send(MouseClick {
+			name: name.to_string(),
+			button: event.button,
+			state: event.state.clone(),
+			pos: Vec2::new( vec.x(), vec.y() ),
+		});
+	}
+}
+
+
 fn detect_mouse_event(
 	mut state: Local<State>,
 	mouse_pos: ResMut<MouseLoc>,
@@ -58,47 +115,36 @@ fn detect_mouse_event(
 	mut my_events: ResMut<Events<MouseClick>>,
 	mouse_button_input_events: Res<Events<MouseButtonInput>>,
 	query: Query<(&SpritePicker, &Sprite, &GlobalTransform)>,
+	hitarea_query: Query<(&HitArea, &GlobalTransform)>,
+	hitarea_notransform_query: Query<With<&HitArea, Without<GlobalTransform>>>,
 	camera_query: Query<(&SpritePickerCamera, &GlobalTransform)>,
 ) {
 	// move mouse click from 0,0 in bottom left and into the centre of screen
-	let point = Vec3::new(
-		mouse_pos.0.x() - (window_size.0.x() / 2.0),
-		mouse_pos.0.y() - (window_size.0.y() / 2.0),
-		0.0,
-	);
+	// let point = Vec3::new(
+		// mouse_pos.0.x() - (window_size.0.x() / 2.0),
+		// mouse_pos.0.y() - (window_size.0.y() / 2.0),
+		// 0.0,
+	// );
 
-	for event in state
-		.mouse_button_event_reader
-		.iter(&mouse_button_input_events)
-	{
-		for (_camera, camera_transform) in camera_query.iter() {
-			let cam_mat = camera_transform.compute_matrix();
-
-			for (sprite_picker, sprite, sprite_transform) in query.iter() {
-				let sprite_mat = sprite_transform.compute_matrix().inverse();
-				let final_mat = sprite_mat * cam_mat;
-				let vec = final_mat.transform_point3(point);
-
-				let half_width = sprite.size.x() / 2.0;
-				let half_height = sprite.size.y() / 2.0;
-				if vec.x() >= -half_width
-					&& vec.x() < half_width
-					&& vec.y() >= -half_height
-					&& vec.y() < half_height
-				{
-					println!(
-						"sprite {} clicked! event: {:?} position: {:?}",
-						sprite_picker.name, event, mouse_pos.0
-					);
-					my_events.send(MouseClick {
-						name: sprite_picker.name.clone(),
-						button: event.button,
-						state: event.state.clone(),
-					});
-				}
-			}
-		}
-	}
+	// for event in state
+		// .mouse_button_event_reader
+		// .iter(&mouse_button_input_events)
+	// {
+		// for (_camera, camera_transform) in camera_query.iter() {
+			// let cam_mat = camera_transform.compute_matrix();
+			// for (sprite_picker, sprite, sprite_transform) in query.iter() {
+				// let sprite_mat = sprite_transform.compute_matrix().inverse() * cam_mat;
+				// process_hitarea( &sprite_picker.name, &sprite.size, &sprite_mat, &point, &event, &mut my_events );
+			// }
+			// for (hitarea, sprite_transform) in hitarea_query.iter() {
+				// let sprite_mat = sprite_transform.compute_matrix().inverse() * cam_mat;
+				// process_hitarea( &hitarea.name, &hitarea.size, &sprite_mat, &point, &event, &mut my_events );
+			// }
+			// for hitarea in hitarea_notransform_query.iter() {
+				// process_hitarea( &hitarea.name, &hitarea.size, &cam_mat, &point, &event, &mut my_events );
+			// }
+		// }
+	// }
 }
 
 fn mouse_movement_updating_system(
